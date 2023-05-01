54ad0b910fcb39f9300cb3979469fdaa3a5db094
Ricardo Silva <ricardo.silva@indico.net.br>
2023-05-01 12:40:55 +0100

    feat: add middleware to verify content-type according to OWASP recommendations

---
+++ middleware/content.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"net/http"
+
+	"api.default.indicoinnovation.pt/entity"
+	"api.default.indicoinnovation.pt/pkg/constants"
+	"api.default.indicoinnovation.pt/pkg/helpers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ValidateContentType() func(context *fiber.Ctx) error {
+	return func(context *fiber.Ctx) error {
+		if context.GetReqHeaders()["Accept"] == "" || !helpers.Contains(constants.AllowedContentTypes, context.GetReqHeaders()["Accept"]) {
+			return helpers.CreateResponse(context, &entity.ErrorResponse{
+				Message:    "Content Not Accepted",
+				StatusCode: http.StatusNotAcceptable,
+			}, http.StatusNotAcceptable)
+		}
+
+		return context.Next()
+	}
+}
