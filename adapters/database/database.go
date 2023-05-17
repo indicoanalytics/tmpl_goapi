1826d37ce9e4104029a2a7fb29678e21f1dcd950
Ricardo Silva <ricardo.silva@indico.net.br>
2023-05-17 17:40:50 +0100

    fix: should send multiple args in function call

--- adapters/database/database.go
+++ adapters/database/database.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Query(query string, outputType interface{}, args ...interface{}) (interface{}, error) {
-	err := app.Inst.DBInstance.Raw(query, args).Scan(&outputType).Error
+	err := app.Inst.DBInstance.Raw(query, args...).Scan(&outputType).Error
 
 	return outputType, err
 }
@@ -13,7 +13,7 @@ func Query(query string, outputType interface{}, args ...interface{}) (interface
 func QueryCount(query string, args ...interface{}) (int, error) {
 	var count int
 
-	err := app.Inst.DBInstance.Raw(query, args).Scan(&count).Error
+	err := app.Inst.DBInstance.Raw(query, args...).Scan(&count).Error
 
 	return count, err
 }
